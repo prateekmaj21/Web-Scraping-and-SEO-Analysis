cf98bb7ea46289b247009e92531045428b890257
Prateek Majumder <52664250+prateekmaj21@users.noreply.github.com>
2024-07-27 16:11:03 +0530

    Update SEO.py

--- SEO.py
+++ SEO.py
@@ -1,6 +1,4 @@
-﻿
-
-#Importing Libraries 
+# Importing Libraries 
 import requests
 from bs4 import BeautifulSoup
 from googlesearch import search
@@ -8,115 +6,92 @@ import re
 from requests_html import HTMLSession
 import streamlit as st
 import warnings
-warnings.filterwarnings('ignore') 
 from PIL import Image
 
+# Suppress warnings
+warnings.filterwarnings('ignore')
 
+# Set up the Streamlit app
 st.title('SEO Analysis App')
-st.markdown("This application helps to analyse the SE0 for a particular Se🔎 arch Engine query .")
+st.markdown("This application helps to analyze the SEO for a particular search engine query 🔎.")
 
+# Display an image
 image = Image.open('seo-1.png')
 st.image(image)
 
+# Provide information about the query
 st.markdown("The query is the string you search in Google.")
 st.markdown("The search here is automated.")
 
+# Input for the search query
 query = st.text_input("Enter the string to be searched")
-'The string entered  by the user is: ', query
+st.write('The string entered by the user is:', query)
 
-link=st.slider("Enter the number of links to be searched",0,100)
-'The number of links that needs to be searched are : ',link
+# Slider to select the number of links to search
+link_count = st.slider("Enter the number of links to be searched", 0, 100)
+st.write('The number of links that need to be searched are:', link_count)
 
-st.markdown("The domain is to be entered, eg co.in, co.uk, .com etc.")
-tld=st.text_input("Enter the top level domain")
-'The top level domain entered by the user is:',tld
+# Input for the top level domain
+st.markdown("The domain is to be entered, e.g., co.in, co.uk, .com etc.")
+tld = st.text_input("Enter the top level domain")
+st.write('The top level domain entered by the user is:', tld)
 
-links=[None]*link
-j=0
+# Initialize variables
+links = []
+body = ""
+
+# Perform the search and display links
 st.subheader("The links searched are:")
-for i in search(query, tld='co.in', lang='en', num=link, start=0, stop=link, pause=2.0):
+for i in search(query, tld=tld, lang='en', num=link_count, start=0, stop=link_count, pause=2.0):
     st.write(i)
-    links[j]=i
-    j=j+1
+    links.append(i)
 
-body=" "
-
-
+# Extract and process the content from each link
 for k in links:
-    html_content = requests.get(k).text
-    soup = BeautifulSoup(html_content, "lxml")
     try:
-        body=body+soup.body.text
-    except:
-        body= body+" "
-    
-meta_tag=soup.findAll('meta')
+        html_content = requests.get(k).text
+        soup = BeautifulSoup(html_content, "lxml")
+        body += soup.body.text if soup.body else " "
+    except Exception as e:
+        st.write(f"Error fetching content from {k}: {e}")
 
+# Process the combined body text
+body = re.sub(r'[^\w\s]', '', body.replace('\n', ' ').replace('\t', ' ').replace('\r', ' ')).lower()
+num = body.count(query.lower())
 
-body= body.replace('\n', ' ')
-body= body.replace('\t', ' ')
-body= body.replace('\r', ' ')
-body=re.sub(r'[^\w\s]', '', body) 
-body=body.lower()
-num=body.count(query)
+st.write("The frequency of the string", query, "in all the link's body combined is:", num)
 
-('-------')
-st.write("The frequency of the string ",query,"in the all the link's body combined is :",num)
-
-title_full=" "
-meta_full=" "
+# Initialize variables for titles and meta descriptions
+title_full = ""
+meta_full = ""
 
 for j in links:
     try:
-        
         session = HTMLSession()
         response = session.get(j)
-    
-    except requests.exceptions.RequestException as e:
-        st.write(e)
-        
+        title = response.html.find('title')
+        title_full += title[0].text if title else " "
+        meta_desc = response.html.xpath('//meta[@name="description"]/@content')
+        meta_full += meta_desc[0] if meta_desc else " "
+    except Exception as e:
+        st.write(f"Error processing link {j}: {e}")
 
-    title =  response.html.find('title')
-    try:
-        
-        title_full= title_full+ (title[0].text)
-    except:
-        title_full= title_full+ " "
-        
-        
-    meta_desc =  response.html.xpath('//meta[@name="description"]/@content')
-    try:
-        meta_full=meta_full+ (meta_desc[0])
-    except:
-        meta_full=meta_full+" "
-    
-    
-    
-meta_full=meta_full.lower()        
-title_full=title_full.lower()
+# Process the combined title and meta description text
+title_full = title_full.lower()
+meta_full = meta_full.lower()
 
-('-------')
- 
-st.subheader ("Title (From all the Links combined)")      
+st.subheader("Title (From all the Links combined)")      
 st.write(title_full)
 
-('-------')
-st.subheader ("Meta  (From all the Links combined)")
+st.subheader("Meta (From all the Links combined)")
 st.write(meta_full)
 
-('-------')
+num2 = title_full.count(query.lower())
+st.write("The frequency of the string", query, "in all the link's titles is:", num2)
 
-num2=title_full.count(query)
-st.write("The frequency of the string ",query,"in all the link's title is :",num2)
-
-('-------')
-
-num3=meta_full.count(query)
-st.write("The frequency of the string ",query,"in all the link's meta is:",num3)
-
-
-('-------')
+num3 = meta_full.count(query.lower())
+st.write("The frequency of the string", query, "in all the link's meta descriptions is:", num3)
 
 st.title("Developed by-")
 st.markdown("Sayanti Dutta & Prateek Majumder")
-st.markdown("All code rights belong to the authors.")
+
